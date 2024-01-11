159c73a4bb810f2d893658aa1af850c63573ff3c
jihunz <thespringsong@gmail.com>
2024-01-11 23:03:15 +0900

    new: 10번 문제 풀이 완료

---
+++ src/Main10.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main10 {
+    public static int solution(int n, int[][] arr) {
+        int answer = 0;
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean flag = true;
+
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[i][j] <= arr[nx][ny]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int[][] arr = new int[x][x];
+
+        for (int i = 0; i < x; i++) {
+            for (int k = 0; k < x; k++) {
+                arr[i][k] = scanner.nextInt();
+            }
+        }
+
+        System.out.print(solution(x, arr));
+    }
+}
