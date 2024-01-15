b9fc079e58f8a76dbb943752bf417daed8809489
jihunz <thespringsong@gmail.com>
2024-01-15 22:39:53 +0900

    new: Two pointer, Sliding window 5번 문제 풀이 완료, 4번 정답 복기

--- src/Main4.java
+++ src/Main4.java
@@ -1,16 +1,29 @@
 import java.util.Scanner;
 
 public class Main4 {
-    public static int solution(int n, int m, int[] arr) {
-        int answer = 0, sum = 0, lt = 0;
+    public static int solution(int n) {
+        int answer = 0, sum = 0, cnt = 0, lt = 1, rt = 1;
 
-        for (int rt = 0; rt < n; rt++) {
-            sum += arr[rt];
-            if (sum == m) answer++;
+        while (true) {
+            if (cnt == 2 && sum > n) {
+                break;
+            }
 
-            while (sum >= m) {
-                sum -= arr[lt++];
-                if (sum == m) answer++;
+            if (sum > n) {
+                rt = ++lt;
+                cnt = 0;
+                sum = 0;
+                continue;
+            }
+
+            sum += rt++;
+            cnt++;
+
+            if (sum == n) {
+                answer++;
+                rt = ++lt;
+                cnt = 0;
+                sum = 0;
             }
         }
 
@@ -20,13 +33,7 @@ public class Main4 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int x = scanner.nextInt();
-        int y = scanner.nextInt();
-        int[] arr = new int[x];
-        for (int i = 0; i < x; i++) {
-            arr[i] = scanner.nextInt();
-        }
-        System.out.println(solution(x, y, arr));
+        System.out.println(solution(x));
     }
 }
---
+++ src/Main5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main5 {
+    public static int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int[] arr = new int[x];
+        for (int i = 0; i < x; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(solution(x, y, arr));
+    }
+}
