5b619fac221cb6a0a740cfa8a6d45fafaeeb8838
jihunz <thespringsong@gmail.com>
2024-01-19 16:07:21 +0900

    new: Stack, Que 5번 문제 풀이 완료

---
+++ src/Main5.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main5 {
+    public static int solution(String str) {
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') stack.push(str.charAt(i));
+            else {
+                stack.pop();
+                if (str.charAt(i-1) == '(') answer += stack.size();
+                else answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        System.out.println(solution(str));
+    }
+}
