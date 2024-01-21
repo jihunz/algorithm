a3fdeacad2db83a82651ba83b7740a92462d48bd
jihunz <thespringsong@gmail.com>
2024-01-21 21:33:29 +0900

    new: Stack, Que 7번 문제 풀이 완료

---
+++ src/Main7.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main7 {
+    public static String solution(String s1, String s2) {
+        String answer = "YES";
+        Queue<Character> queue = new LinkedList<>();
+
+        for (char x : s1.toCharArray()) queue.offer(x);
+        for (char x : s2.toCharArray()) {
+            if (queue.contains(x)) {
+                if (x != queue.poll()) return "NO";
+            }
+        }
+
+        if (!queue.isEmpty()) return "NO";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String n = scanner.next();
+        String m = scanner.next();
+        System.out.println(solution(n, m));
+    }
+}
