e38755d8b9e0b370e36d799514045f8cb73dfa6f
jihunz <thespringsong@gmail.com>
2024-01-26 00:03:52 +0900

    new: Sorting and searching 10번 - 이분검색을 이용한 마구간 최대 거리 구하기 문제 풀이 완료

---
+++ src/Main10.java
@@ -0,0 +1,47 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main10 {
+    public static int solution(int size, int num, int[] arr) {
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 1;
+        int rt = arr[size - 1];
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+
+            if (count(arr, mid) >= num) {
+                answer = mid;
+                lt = mid + 1;
+            } else {
+                rt = mid - 1;
+            }
+        }
+        return answer;
+    }
+
+    private static int count(int[] arr, int dist) {
+        int cnt = 1;
+        int ep = arr[0];
+
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i]-ep >= dist) {
+                cnt++;
+                ep = arr[i];
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int[] arr = new int[a];
+        for (int i = 0; i < a; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(solution(a, b, arr));
+    }
+}
