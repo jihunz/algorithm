c4b49084f51dd9b29106cb42568bbdb026f051ee
jihunz <thespringsong@gmail.com>
2024-01-26 21:15:48 +0900

    new: 재귀함수 -> 이진수 구하기 문제 풀이 완료

---
+++ src/Main2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main2 {
+    public static void recursive(int x) {
+        if (x == 0) return;
+        else {
+            recursive(x/2);
+            System.out.print(x % 2 + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        recursive(x);
+    }
+}
