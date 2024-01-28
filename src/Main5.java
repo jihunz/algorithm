df14f29f3dfd280313b8f72f0954ac6280f60999
jihunz <thespringsong@gmail.com>
2024-01-28 16:15:15 +0900

    new: dfs -> 5번 전위순회 풀이 완료

---
+++ src/Main5.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node lt, rt;
+
+    public Node(int val) {
+        this.data = val;
+        lt = rt = null;
+    }
+}
+
+public class Main5 {
+    static Node root;
+
+    public static void dfs(Node root) {
+        if (root == null) return;
+        else {
+            System.out.print(root.data + " ");
+            dfs(root.lt);
+            dfs(root.rt);
+        }
+    }
+
+    public static void main(String[] args) {
+        root = new Node(1);
+        root.lt = new Node(2);
+        root.rt = new Node(3);
+        root.lt.lt = new Node(4);
+        root.lt.rt = new Node(5);
+        root.rt.lt = new Node(6);
+        root.rt.rt = new Node(7);
+        dfs(root);
+
+
+    }
+}
