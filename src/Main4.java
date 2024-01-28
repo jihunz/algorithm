8b4261070b95e0fab38d3458a515703c7b64d32e
jihunz <thespringsong@gmail.com>
2024-01-28 15:54:21 +0900

    new: 재귀함수 -> 4번 메모리제이션으로 수정

--- src/Main4.java
+++ src/Main4.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
 
 public class Main4 {
+    static int[] fibo;
+
     public static int recursive(int x) {
-        if (x == 1) return 1;
-        else if (x == 2) return 1;
+        if (fibo[x] > 0) return fibo[x];
+
+        if (x == 1) return fibo[x] = 1;
+        else if (x == 2) return fibo[x] = 1;
         else {
-            return recursive(x-2) + recursive(x-1);
+            return fibo[x] = recursive(x - 2) + recursive(x - 1);
         }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
-        for (int i=1; i<=x; i++) System.out.print(recursive(i) + " ");
+        fibo = new int[x + 1];
+        recursive(x);
+        for (int i = 1; i <= x; i++) System.out.print(fibo[i] + " ");
     }
 }
