5372d4e06970b7457dfd359d9f523adeba07d75a
jihunz <thespringsong@gmail.com>
2024-02-01 21:52:48 +0900

    new: 11번 인접행렬을 바탕으로 종점 노드까지 갈 수 있는 경우의 수 -> DFS로 풀이

--- src/Main11.java
+++ src/Main11.java
@@ -1,42 +1,39 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
-class Node6 {
-    int val;
-    Node6 lt, rt;
-
-    public Node6(int val) {
-        this.val = val;
-        lt = rt = null;
-    }
-}
+import java.util.Scanner;
 
 public class Main11 {
-    public static int bfs(Node6 root) {
-        Queue<Node6> q = new LinkedList<>();
-        q.offer(root);
-        int level = 0;
+    static int node, edge, answer = 0;
+    static int[][] graph;
+    static int[] chk;
 
-        while (!q.isEmpty()) {
-            int len = q.size();
-            for (int i = 0; i < len; i++) {
-                Node6 curr = q.poll();
-                if (curr.lt == null && curr.rt == null) return level;
-                if (curr.lt != null) q.offer(curr.lt);
-                if (curr.rt != null) q.offer(curr.rt);
+
+    public static void dfs(int val) {
+        if (val == node) answer++;
+        else {
+            for (int i = 1; i <= node; i++) {
+                if (graph[val][i] == 1 && chk[i] == 0) {
+                    chk[i] = 1;
+                    dfs(i);
+                    chk[i] = 0;
+                }
             }
-            level++;
         }
-        return 0;
     }
 
     public static void main(String[] args) {
-        Node6 root = new Node6(1);
-        root.lt = new Node6(2);
-        root.rt = new Node6(3);
-        root.lt.lt = new Node6(4);
-        root.lt.rt = new Node6(5);
-        System.out.println(bfs(root));
+        Scanner scanner = new Scanner(System.in);
+        node = scanner.nextInt();
+        edge = scanner.nextInt();
+        graph = new int[node + 1][node + 1];
+        chk = new int[node + 1];
 
+        for (int i = 0; i < edge; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            graph[a][b] = 1;
+        }
+
+        chk[1] = 1;
+        dfs(1);
+        System.out.println(answer);
     }
 }
