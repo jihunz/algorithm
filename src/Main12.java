0622c54d8ec5999aefd2579560794fadc343d143
jihunz <thespringsong@gmail.com>
2024-02-02 17:43:04 +0900

    new: DFS -> 13번 인접리스트를 이용하여 풀이 완료

---
+++ src/Main12.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main12 {
+    static int node, edge, answer = 0;
+    static int[][] graph;
+    static int[] chk;
+
+
+    public static void dfs(int val) {
+        if (val == node) answer++;
+        else {
+            for (int i = 1; i <= node; i++) {
+                if (graph[val][i] == 1 && chk[i] == 0) {
+                    chk[i] = 1;
+                    dfs(i);
+                    chk[i] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        node = scanner.nextInt();
+        edge = scanner.nextInt();
+        graph = new int[node + 1][node + 1];
+        chk = new int[node + 1];
+
+        for (int i = 0; i < edge; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            graph[a][b] = 1;
+        }
+
+        chk[1] = 1;
+        dfs(1);
+        System.out.println(answer);
+    }
+}
--- src/Main13.java
+++ src/Main13.java
@@ -1,19 +1,20 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main11 {
+public class Main13 {
     static int node, edge, answer = 0;
-    static int[][] graph;
+    static ArrayList<ArrayList<Integer>> graph;
     static int[] chk;
 
 
     public static void dfs(int val) {
         if (val == node) answer++;
         else {
-            for (int i = 1; i <= node; i++) {
-                if (graph[val][i] == 1 && chk[i] == 0) {
-                    chk[i] = 1;
-                    dfs(i);
-                    chk[i] = 0;
+            for (int nv : graph.get(val)) {
+                if (chk[nv] == 0) {
+                    chk[nv] = 1;
+                    dfs(nv);
+                    chk[nv] = 0;
                 }
             }
         }
@@ -23,17 +24,21 @@ public class Main11 {
         Scanner scanner = new Scanner(System.in);
         node = scanner.nextInt();
         edge = scanner.nextInt();
-        graph = new int[node + 1][node + 1];
         chk = new int[node + 1];
 
+        graph = new ArrayList<>();
+        for (int i = 0; i <= node; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
         for (int i = 0; i < edge; i++) {
             int a = scanner.nextInt();
             int b = scanner.nextInt();
-            graph[a][b] = 1;
+            graph.get(a).add(b);
         }
 
         chk[1] = 1;
         dfs(1);
         System.out.println(answer);
+
     }
 }
