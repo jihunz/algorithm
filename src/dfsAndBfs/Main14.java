669c328f184c0e9e8ad6b342f5b686b33f74b032
jihunz <thespringsong@gmail.com>
2024-02-04 17:38:11 +0900

    new: BFS -> 14번 풀이 완료

--- src/dfsAndBfs/Main14.java
+++ src/dfsAndBfs/Main14.java
@@ -1,22 +1,39 @@
 package dfsAndBfs;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main14 {
     static int node, edge, answer = 0;
     static ArrayList<ArrayList<Integer>> graph;
+    static int[] chk;
     static int[] dis;
 
 
-    public static void dfs(int val) {
-        if (val == node) answer++;
+    public static void bfs(int v) {
+        chk[v] = 1;
+        dis[v] = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(v);
+        while (!queue.isEmpty()) {
+            int cv = queue.poll();
+            for (int nv : graph.get(cv)) {
+                if (chk[nv] == 0) {
+                    chk[nv] = 1;
+                    queue.offer(nv);
+                    dis[nv] = dis[cv] + 1;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         node = scanner.nextInt();
         edge = scanner.nextInt();
+        chk = new int[node + 1];
         dis = new int[node + 1];
 
         graph = new ArrayList<>();
@@ -29,7 +46,7 @@ public class Main14 {
             graph.get(a).add(b);
         }
 
-        dfs(1);
+        bfs(1);
 
         for (int i = 2; i <= node; i++) {
             System.out.println(i + " : " + dis[i]);
