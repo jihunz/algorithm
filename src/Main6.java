d77d0bd7ece00b6dca85ff0d5bf0370fd47aba02
jihunz <thespringsong@gmail.com>
2024-02-09 16:10:41 +0900

    new: DFS -> 8-6 순열 구하기 풀이 완료

---
+++ src/Main6.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main6 {
+    static int[] pm, chk, arr;
+    static int n, m;
+
+    public static void dfs(int level) {
+        if (level == m) {
+            for (int x : pm) System.out.print(x + " ");
+            System.out.println();
+        } else {
+            for (int i = 0; i < n; i++) {
+                if (chk[i] == 0) {
+                    chk[i] = 1;
+                    pm[level] = arr[i];
+                    dfs(level + 1);
+                    chk[i] = 0;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        m = scanner.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        chk = new int[n];
+        pm = new int[m];
+        dfs(0);
+    }
+}
