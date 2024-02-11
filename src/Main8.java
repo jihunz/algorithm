472e1e6e2c6c8fb547f648701157f5360165ec14
jihunz <thespringsong@gmail.com>
2024-02-11 14:43:36 +0900

    new: DFS -> 8-9 조합 만들기 풀이 완료

--- src/Main8.java
+++ src/Main8.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main8 {
     static int[] b, p, chk;
-    static int n, f;
+    static int num, target;
     static boolean flag = false;
     static int[][] dy = new int[35][35];
 
@@ -14,13 +14,13 @@ public class Main8 {
 
     public static void dfs(int level, int sum) {
         if (flag) return;
-        if (level == n) {
-            if (sum == f) {
+        if (level == num) {
+            if (sum == target) {
                 for (int x : p) System.out.print(x + " ");
                 flag = true;
             }
         } else {
-            for (int i = 1; i <= n; i++) {
+            for (int i = 1; i <= num; i++) {
                 if (chk[i] == 0) {
                     chk[i] = 1;
                     p[level] = i;
@@ -33,13 +33,13 @@ public class Main8 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        f = scanner.nextInt();
-        b = new int[n];
-        p = new int[n];
-        chk = new int[n + 1];
-        for (int i = 0; i < n; i++) {
-            b[i] = combi(n - 1, i);
+        num = scanner.nextInt();
+        target = scanner.nextInt();
+        b = new int[num];
+        p = new int[num];
+        chk = new int[num + 1];
+        for (int i = 0; i < num; i++) {
+            b[i] = combi(num - 1, i);
         }
         dfs(0, 0);
     }
---
+++ src/Main9.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main9 {
+    static int num, pickNum;
+    static int[] answer;
+
+    public static void dfs(int level, int currNum) {
+        if (level == pickNum) {
+            for (int x : answer) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        } else {
+            for (int i = currNum; i <= num; i++) {
+                answer[level] = i;
+                dfs(level + 1, i + 1);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        num = scanner.nextInt();
+        pickNum = scanner.nextInt();
+        answer = new int[pickNum];
+        dfs(0, 1);
+    }
+}
