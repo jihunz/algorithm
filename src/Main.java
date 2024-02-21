97580025526f50c9e075339c80d94bb1fad9c46c
jihunz <thespringsong@gmail.com>
2024-02-21 16:37:08 +0900

    new: dynamic programming -> 계단 오르기 -> 피보나치 수열로 목표 계단까지의 경우의 수 구하기 풀이 완료

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,22 @@
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
-
-class Body implements Comparable<Body> {
-    public int h, w;
-
-    public Body(int h, int w) {
-        this.h = h;
-        this.w = w;
-    }
-
-    @Override
-    public int compareTo(Body body) {
-        return body.h - this.h;
-    }
-}
-
 public class Main {
-    static int solution(ArrayList<Body> list, int n) {
-        int cnt = 0;
-        Collections.sort(list);
-        int max = Integer.MIN_VALUE;
-        for (Body body : list) {
-            if (body.w > max) {
-                max = body.w;
-                cnt++;
-            }
-        }
-        return cnt;
-    }
-
+    static int[] dy;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        ArrayList<Body> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            int h = scanner.nextInt();
-            int w = scanner.nextInt();
-            list.add(new Body(h, w));
-        }
-        System.out.println(solution(list, n));
-
+        dy = new int[n + 1];
+        System.out.println(solution(n));
     }
 
+    static int solution(int n) {
+        dy[1] = 1;
+        dy[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dy[i] = dy[i - 2] + dy[i - 1];
+        }
 
+        return dy[n];
+    }
 }
